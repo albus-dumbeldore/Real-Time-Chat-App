e363e6c18be2c84640c4b41ab02c2de40e48f352
albus-dumbeldore <dhruv0426@gmail.com>
2020-04-23 15:22:54 +0530

    change admin message

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ io.on('connection',(socket)=>{
         const user = removeUser(socket.id)
 
         if(user){ 
-            io.to(user.room).emit('message',generateMessage('Admin '+`${user.username} has left!`))
+            io.to(user.room).emit('message',generateMessage('Admin ',`${user.username} has left!`))
             io.to(user.room).emit('roomData',{
                 room:user.room,
                 users:getUsersInRoom(user.room)
